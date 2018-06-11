0b350803ff48768a11d06169b8fbed0431fc7469
Cecille Salazar <cecillersalazar@gmail.com>
2018-06-11 12:43:42 -0700

    Separated yearOfBirth function

--- drills.js
+++ drills.js
@@ -1,6 +1,12 @@
-function whoAmI(name, age) {
+function yearOfBirth(age) {
     const yearOfBirth = 2018 - age;
-    console.log(`Hi, my name is ${name} and Im ${age} years old`);
-    console.log(`I was born in ${yearOfBirth}.`);
+    return yearOfBirth;
 }
+
+function whoAmI(name, age) {
+    const yob = yearOfBirth(age);
+    console.log(`Hi, my name is ${name} and Im ${age} years old`);
+    console.log(`I was born in ${yob}`);
+}
+
 whoAmI('Cecille', 26);
