cf5ef0dab56830764096947527447454f22c6c42
Filipp Gorbunov <Filippgorb@gmail.com>
2018-06-11 12:31:58 -0700

    Completed part 4

--- drills.js
+++ drills.js
@@ -1,4 +1,6 @@
 function whoAmI(name, age) {
+    const yearOfBirth = 2018 - age;
     console.log(`Hi, my name is ${name} and Im ${age} years old`);
+    console.log(`I was born in ${yearOfBirth}.`);
 }
 whoAmI('Cecille', 26);
